927d83794187361435f02615e0748f22fb080185
Drew Naylor <DrewNaylor@users.noreply.github.com>
2019-07-13 15:29:39 -0400

    Name all the rest of the controls + resize button area...
    and call it "Search".

--- HTML-Character-Code-Acquisition-Program/aaformMainWindow.Designer.vb
+++ HTML-Character-Code-Acquisition-Program/aaformMainWindow.Designer.vb
@@ -30,10 +30,10 @@ Partial Class aaformMainWindow
         Me.menubarToolsMenuOptionsButton = New System.Windows.Forms.ToolStripMenuItem()
         Me.menubarHelpMenu = New System.Windows.Forms.ToolStripMenuItem()
         Me.menubarHelpMenuAboutButton = New System.Windows.Forms.ToolStripMenuItem()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
-        Me.Button1 = New System.Windows.Forms.Button()
+        Me.textboxInput = New System.Windows.Forms.TextBox()
+        Me.buttonSearch = New System.Windows.Forms.Button()
         Me.TableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel()
-        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.textboxOutput = New System.Windows.Forms.TextBox()
         Me.menubarMainWindow.SuspendLayout()
         Me.TableLayoutPanel1.SuspendLayout()
         Me.SuspendLayout()
@@ -64,7 +64,7 @@ Partial Class aaformMainWindow
         'menubarFileMenuExitButton
         '
         Me.menubarFileMenuExitButton.Name = "menubarFileMenuExitButton"
-        Me.menubarFileMenuExitButton.Size = New System.Drawing.Size(152, 22)
+        Me.menubarFileMenuExitButton.Size = New System.Drawing.Size(92, 22)
         Me.menubarFileMenuExitButton.Text = "&Exit"
         '
         'menubarToolsMenu
@@ -77,7 +77,7 @@ Partial Class aaformMainWindow
         'menubarToolsMenuOptionsButton
         '
         Me.menubarToolsMenuOptionsButton.Name = "menubarToolsMenuOptionsButton"
-        Me.menubarToolsMenuOptionsButton.Size = New System.Drawing.Size(152, 22)
+        Me.menubarToolsMenuOptionsButton.Size = New System.Drawing.Size(116, 22)
         Me.menubarToolsMenuOptionsButton.Text = "&Options"
         '
         'menubarHelpMenu
@@ -90,35 +90,35 @@ Partial Class aaformMainWindow
         'menubarHelpMenuAboutButton
         '
         Me.menubarHelpMenuAboutButton.Name = "menubarHelpMenuAboutButton"
-        Me.menubarHelpMenuAboutButton.Size = New System.Drawing.Size(152, 22)
+        Me.menubarHelpMenuAboutButton.Size = New System.Drawing.Size(107, 22)
         Me.menubarHelpMenuAboutButton.Text = "&About"
         '
-        'TextBox1
+        'textboxInput
         '
-        Me.TextBox1.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.TextBox1.Location = New System.Drawing.Point(3, 3)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(303, 20)
-        Me.TextBox1.TabIndex = 0
+        Me.textboxInput.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.textboxInput.Location = New System.Drawing.Point(3, 3)
+        Me.textboxInput.Name = "textboxInput"
+        Me.textboxInput.Size = New System.Drawing.Size(323, 20)
+        Me.textboxInput.TabIndex = 0
         '
-        'Button1
+        'buttonSearch
         '
-        Me.Button1.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.Button1.Location = New System.Drawing.Point(312, 3)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(85, 22)
-        Me.Button1.TabIndex = 1
-        Me.Button1.Text = "Button1"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.buttonSearch.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.buttonSearch.Location = New System.Drawing.Point(332, 3)
+        Me.buttonSearch.Name = "buttonSearch"
+        Me.buttonSearch.Size = New System.Drawing.Size(65, 22)
+        Me.buttonSearch.TabIndex = 1
+        Me.buttonSearch.Text = "Search"
+        Me.buttonSearch.UseVisualStyleBackColor = True
         '
         'TableLayoutPanel1
         '
         Me.TableLayoutPanel1.ColumnCount = 2
         Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100.0!))
-        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 91.0!))
-        Me.TableLayoutPanel1.Controls.Add(Me.Button1, 1, 0)
-        Me.TableLayoutPanel1.Controls.Add(Me.TextBox1, 0, 0)
-        Me.TableLayoutPanel1.Controls.Add(Me.TextBox2, 0, 1)
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 71.0!))
+        Me.TableLayoutPanel1.Controls.Add(Me.buttonSearch, 1, 0)
+        Me.TableLayoutPanel1.Controls.Add(Me.textboxInput, 0, 0)
+        Me.TableLayoutPanel1.Controls.Add(Me.textboxOutput, 0, 1)
         Me.TableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill
         Me.TableLayoutPanel1.Location = New System.Drawing.Point(0, 24)
         Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
@@ -128,17 +128,17 @@ Partial Class aaformMainWindow
         Me.TableLayoutPanel1.Size = New System.Drawing.Size(400, 280)
         Me.TableLayoutPanel1.TabIndex = 3
         '
-        'TextBox2
+        'textboxOutput
         '
-        Me.TableLayoutPanel1.SetColumnSpan(Me.TextBox2, 2)
-        Me.TextBox2.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.TextBox2.Location = New System.Drawing.Point(3, 31)
-        Me.TextBox2.Multiline = True
-        Me.TextBox2.Name = "TextBox2"
-        Me.TextBox2.ReadOnly = True
-        Me.TextBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
-        Me.TextBox2.Size = New System.Drawing.Size(394, 246)
-        Me.TextBox2.TabIndex = 2
+        Me.TableLayoutPanel1.SetColumnSpan(Me.textboxOutput, 2)
+        Me.textboxOutput.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.textboxOutput.Location = New System.Drawing.Point(3, 31)
+        Me.textboxOutput.Multiline = True
+        Me.textboxOutput.Name = "textboxOutput"
+        Me.textboxOutput.ReadOnly = True
+        Me.textboxOutput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.textboxOutput.Size = New System.Drawing.Size(394, 246)
+        Me.textboxOutput.TabIndex = 2
         '
         'aaformMainWindow
         '
@@ -168,8 +168,8 @@ Partial Class aaformMainWindow
     Friend WithEvents menubarToolsMenuOptionsButton As ToolStripMenuItem
     Friend WithEvents menubarHelpMenu As ToolStripMenuItem
     Friend WithEvents menubarHelpMenuAboutButton As ToolStripMenuItem
-    Friend WithEvents TextBox1 As TextBox
-    Friend WithEvents Button1 As Button
+    Friend WithEvents textboxInput As TextBox
+    Friend WithEvents buttonSearch As Button
     Friend WithEvents TableLayoutPanel1 As TableLayoutPanel
-    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents textboxOutput As TextBox
 End Class
