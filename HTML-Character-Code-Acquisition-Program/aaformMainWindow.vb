94b05f5ef67b87a9495e20b3e66f6bbbb82b743b
Drew Naylor <DrewNaylor@users.noreply.github.com>
2019-07-13 15:40:56 -0400

    Uncomment code that does stuff and it works!
    Just need to change variable names to more-obvious names and add comments. Also more features like search when typing and search on Enter need to be added. It would be nice to have an option to hide the Search button if the user wants to as well.

--- HTML-Character-Code-Acquisition-Program/aaformMainWindow.vb
+++ HTML-Character-Code-Acquisition-Program/aaformMainWindow.vb
@@ -1,23 +1,27 @@
 ﻿Imports System.Xml
 
 Public Class aaformMainWindow
+    Private Sub buttonSearch_Click(sender As Object, e As EventArgs) Handles buttonSearch.Click
+        textboxOutput.Clear()
+        getCodes()
+    End Sub
 
-    'Private Shared fileToSearchFrom As XmlDocument = New XmlDocument()
-    'Private Shared searchNamespaceManager As New XmlNamespaceManager(fileToSearchFrom.NameTable)
+    Private Shared fileToSearchFrom As XmlDocument = New XmlDocument()
+    Private Shared searchNamespaceManager As New XmlNamespaceManager(fileToSearchFrom.NameTable)
 
-    'Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-    '    searchNamespaceManager.AddNamespace("hccap", "https://drewnaylor.github.io/xml")
-    '    Debug.WriteLine(IO.Directory.GetCurrentDirectory & "\searchXml.xml")
-    '    fileToSearchFrom.Load(IO.Directory.GetCurrentDirectory & "\searchXml.xml")
-    '    Debug.WriteLine(fileToSearchFrom.OuterXml.ToString.Replace("_", "&"))
-    'End Sub
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        searchNamespaceManager.AddNamespace("hccap", "https://drewnaylor.github.io/xml")
+        Debug.WriteLine(IO.Directory.GetCurrentDirectory & "\searchXml.xml")
+        fileToSearchFrom.Load(IO.Directory.GetCurrentDirectory & "\searchXml.xml")
+        Debug.WriteLine(fileToSearchFrom.OuterXml.ToString.Replace("_", "&"))
+    End Sub
 
-    'Private Sub getCodes()
-    '    For Each node As XmlNode In fileToSearchFrom.SelectSingleNode("/root/characterCodeSection")
-    '        Debug.WriteLine("node.InnerText: " & node.InnerText)
-    '        If node.InnerText.ToLowerInvariant.Contains(TextBox1.Text.ToLowerInvariant) Then
-    '            TextBox2.AppendText(node.InnerText.Replace("_", "&") & vbCrLf)
-    '        End If
-    '    Next
-    'End Sub
+    Private Sub getCodes()
+        For Each node As XmlNode In fileToSearchFrom.SelectSingleNode("/root/characterCodeSection")
+            Debug.WriteLine("node.InnerText: " & node.InnerText)
+            If node.InnerText.ToLowerInvariant.Contains(textboxInput.Text.ToLowerInvariant) Then
+                textboxOutput.AppendText(node.InnerText.Replace("_", "&") & vbCrLf)
+            End If
+        Next
+    End Sub
 End Class
