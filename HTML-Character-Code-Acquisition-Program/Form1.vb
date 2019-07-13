a94b011cbead2668c982f92569b073fc98afbe62
Drew Naylor <DrewNaylor@users.noreply.github.com>
2019-07-13 14:30:29 -0400

    Paste in most of the code to get character codes from XML file.
    This code is from a prototype version that I made before deciding that this program would work and that it should be made properly as a new project file rather than using the prototype version.
    
    The variable names need to be changed to make more sense, comments need to be added, and the boilerplate needs to be added yet.

--- HTML-Character-Code-Acquisition-Program/Form1.vb
+++ HTML-Character-Code-Acquisition-Program/Form1.vb
@@ -1,3 +1,23 @@
-﻿Public Class Form1
+﻿Imports System.Xml
 
+Public Class Form1
+
+    Private Shared fileToSearchFrom As XmlDocument = New XmlDocument()
+    Private Shared searchNamespaceManager As New XmlNamespaceManager(fileToSearchFrom.NameTable)
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        searchNamespaceManager.AddNamespace("hccap", "https://drewnaylor.github.io/xml")
+        Debug.WriteLine(IO.Directory.GetCurrentDirectory & "\searchXml.xml")
+        fileToSearchFrom.Load(IO.Directory.GetCurrentDirectory & "\searchXml.xml")
+        Debug.WriteLine(fileToSearchFrom.OuterXml.ToString.Replace("_", "&"))
+    End Sub
+
+    Private Sub getCodes()
+        For Each node As XmlNode In fileToSearchFrom.SelectSingleNode("/root/characterCodeSection")
+            Debug.WriteLine("node.InnerText: " & node.InnerText)
+            If node.InnerText.ToLowerInvariant.Contains(TextBox1.Text.ToLowerInvariant) Then
+                TextBox2.AppendText(node.InnerText.Replace("_", "&") & vbCrLf)
+            End If
+        Next
+    End Sub
 End Class
