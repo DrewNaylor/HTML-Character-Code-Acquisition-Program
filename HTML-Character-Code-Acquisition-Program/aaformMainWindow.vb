007a43a6da7e9a81f71739359c05a7bb2e3f8082
Drew Naylor <DrewNaylor@users.noreply.github.com>
2019-07-13 16:29:30 -0400

    Add a bunch of more comments + rename "node" to...
    "characterNode".

--- HTML-Character-Code-Acquisition-Program/aaformMainWindow.vb
+++ HTML-Character-Code-Acquisition-Program/aaformMainWindow.vb
@@ -10,17 +10,30 @@ Public Class aaformMainWindow
     Private Shared searchNamespaceManager As New XmlNamespaceManager(xmlFileToSearch.NameTable)
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Add a namespace to the namespace manager so that XML works properly.
         searchNamespaceManager.AddNamespace("html-ccap", "https://drewnaylor.github.io/xml")
         Debug.WriteLine(IO.Directory.GetCurrentDirectory & "\searchXml.xml")
+        ' Load in the XML file with the character codes from the current directory.
+        ' This would preferably also have an option to load it from the
+        ' application's Resources, but the XML file isn't close enough
+        ' to completion for that to make sense.
         xmlFileToSearch.Load(IO.Directory.GetCurrentDirectory & "\searchXml.xml")
         Debug.WriteLine(xmlFileToSearch.OuterXml.ToString.Replace("_", "&"))
     End Sub
 
     Private Sub getCodes()
-        For Each node As XmlNode In xmlFileToSearch.SelectSingleNode("/root/characterCodeSection")
-            Debug.WriteLine("node.InnerText: " & node.InnerText)
-            If node.InnerText.ToLowerInvariant.Contains(textboxInput.Text.ToLowerInvariant) Then
-                textboxOutput.AppendText(node.InnerText.Replace("_", "&") & vbCrLf)
+        'First we look at each XmlNode within the character code section node.
+        For Each characterNode As XmlNode In xmlFileToSearch.SelectSingleNode("/root/characterCodeSection")
+            Debug.WriteLine("node.InnerText: " & characterNode.InnerText)
+            ' If it's determined that the text in the node contains the text in the textbox,
+            ' then that is appended to the output textbox.
+            ' To compare the text, both the text in the textbox and the text in the node
+            ' are made to be lowercase and culture/language-invariant.
+            If characterNode.InnerText.ToLowerInvariant.Contains(textboxInput.Text.ToLowerInvariant) Then
+                ' Underscores are replaced with and symbols so that they match how HTML expects
+                ' character codes to look. The underscores are necessary for now as XML complains
+                ' when they're in the file, so this is a workaround.
+                textboxOutput.AppendText(characterNode.InnerText.Replace("_", "&") & vbCrLf)
             End If
         Next
         ' Now trim the end.
