efce6fa7e1b1afe3301023f6b8149abe86a3eec9
Drew Naylor <DrewNaylor@users.noreply.github.com>
2019-07-16 21:28:46 -0400

    Start adding to Options window codebehind.

--- HTML-Character-Code-Acquisition-Program/aaformOptionsWindow.vb
+++ HTML-Character-Code-Acquisition-Program/aaformOptionsWindow.vb
@@ -1,3 +1,32 @@
-﻿Public Class aaformOptionsWindow
+﻿' HTML Character Code Acquisition Program - Developer tool to look up HTML
+' character codes/HTML character entities.
+' Copyright (C) 2019 Drew Naylor
+' (Note that the copyright years include the years left out by the hyphen.)
+'
+' This file is part of HTML Character Code Acquisition Program
+' (Program is also known as "html-ccap".)
+'
+' html-ccap is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+'
+' html-ccap is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+'
+' You should have received a copy of the GNU General Public License
+' along with html-ccap.  If not, see <http://www.gnu.org/licenses/>.
 
+
+
+Public Class aaformOptionsWindow
+    Private Sub buttonCancel_Click(sender As Object, e As EventArgs) Handles buttonCancel.Click
+
+    End Sub
+
+    Private Sub aaformOptionsWindow_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
 End Class
