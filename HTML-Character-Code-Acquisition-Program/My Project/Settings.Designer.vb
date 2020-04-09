1bfb94a4605ad51485097e50e146716f7432076a
Drew Naylor <DrewNaylor@users.noreply.github.com>
2020-04-09 16:20:26 -0400

    Add setting to allow searching from start of what's...
    entered.

--- HTML-Character-Code-Acquisition-Program/My Project/Settings.Designer.vb
+++ HTML-Character-Code-Acquisition-Program/My Project/Settings.Designer.vb
@@ -161,6 +161,18 @@ Namespace My
                 Me("showNumericCharacterCode") = value
             End Set
         End Property
+        
+        <Global.System.Configuration.UserScopedSettingAttribute(),  _
+         Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
+         Global.System.Configuration.DefaultSettingValueAttribute("False")>  _
+        Public Property searchFromStart() As Boolean
+            Get
+                Return CType(Me("searchFromStart"),Boolean)
+            End Get
+            Set
+                Me("searchFromStart") = value
+            End Set
+        End Property
     End Class
 End Namespace
 
