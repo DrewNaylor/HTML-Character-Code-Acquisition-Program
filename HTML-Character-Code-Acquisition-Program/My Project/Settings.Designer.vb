65c8863aa7798df232560bb419ea3fd137cc0901
Drew Naylor <DrewNaylor@users.noreply.github.com>
2020-04-09 16:17:16 -0400

    Also add a setting to allow users to have the numeric...
    character code shown instead of the word one for characters that have both. For example, instead of showing &amp;, &#38; will be shown. If a character only has a numeric code, this won't apply and only the numeric code will be shown.

--- HTML-Character-Code-Acquisition-Program/My Project/Settings.Designer.vb
+++ HTML-Character-Code-Acquisition-Program/My Project/Settings.Designer.vb
@@ -149,6 +149,18 @@ Namespace My
                 Me("showMatchingAlias") = value
             End Set
         End Property
+        
+        <Global.System.Configuration.UserScopedSettingAttribute(),  _
+         Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
+         Global.System.Configuration.DefaultSettingValueAttribute("False")>  _
+        Public Property showNumericCharacterCode() As Boolean
+            Get
+                Return CType(Me("showNumericCharacterCode"),Boolean)
+            End Get
+            Set
+                Me("showNumericCharacterCode") = value
+            End Set
+        End Property
     End Class
 End Namespace
 
