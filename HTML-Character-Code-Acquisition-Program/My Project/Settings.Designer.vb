b59190f0fe5fc1eff1b5830d7b22ca5219823aa8
Drew Naylor <DrewNaylor@users.noreply.github.com>
2020-04-09 15:50:56 -0400

    Add (C) alias for copyright + prepare to add setting...
    to show the matching alias in search results.

--- HTML-Character-Code-Acquisition-Program/My Project/Settings.Designer.vb
+++ HTML-Character-Code-Acquisition-Program/My Project/Settings.Designer.vb
@@ -137,6 +137,18 @@ Namespace My
                 Me("searchIsFilter") = value
             End Set
         End Property
+        
+        <Global.System.Configuration.UserScopedSettingAttribute(),  _
+         Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
+         Global.System.Configuration.DefaultSettingValueAttribute("False")>  _
+        Public Property showMatchingAlias() As Boolean
+            Get
+                Return CType(Me("showMatchingAlias"),Boolean)
+            End Get
+            Set
+                Me("showMatchingAlias") = value
+            End Set
+        End Property
     End Class
 End Namespace
 
